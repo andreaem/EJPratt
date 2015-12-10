e4e8b8056afed32f3886d5e11cd86cf00d86572a
root <root@vm-paypal.vulcan.lan>
2015-12-10 17:00:01 -0500

    added the index.php file which sets the dates

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form name="datePicker" action="paypal.php" method="get">
+Start Date: <input type="date" name="startDate">
+End Date: <input type="date" name="endDate">
+<input type="submit" value="Submit">
+<br>
+Edited March 5th 2015
+
+</body>
+</html>
+~
+
